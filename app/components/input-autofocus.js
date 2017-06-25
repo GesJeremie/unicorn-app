37e81e2f3bb4aafc485ecc1e15e38995a349b55b
Jeremie Ges <ges.jeremie@gmail.com>
2017-06-25 10:37:30 +0300

    Don't auto focus inputs on small viewport

--- app/components/input-autofocus.js
+++ app/components/input-autofocus.js
@@ -4,7 +4,13 @@ export default Ember.Component.extend({
   target: null,
 
   didInsertElement() {
-    let target = this.get('target');
+    let target = this.get('target'),
+        media = this.get('media');
+
+    if (media.get('isSmall') || media.get('isMedium')) {
+      return;
+    }
+
     this.$(target).focus();
   }
 });
