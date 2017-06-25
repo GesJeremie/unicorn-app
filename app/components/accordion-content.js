c7c0da0840a003c05dbfc68c3a1fbb452525ab6f
Jeremie Ges <ges.jeremie@gmail.com>
2017-06-25 07:47:47 +0300

    Tweaks home page

--- app/components/accordion-content.js
+++ app/components/accordion-content.js
@@ -3,23 +3,46 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   isShown: false,
+  classOpened: '',
 
-  mediaSmallChanged: Ember.observer('media.isSmall', function() {
-    console.log('media changed');
-    if (this.get('media.isSmall')) {
+  mediaSmallChanged: Ember.observer('media.isSmall', 'media.isMedium', function() {
+    this.setupIsShown();
+  }),
+
+  updateClassOpened: Ember.observer('isShown', 'media.isSmall', 'media.isMedium', function() {
+
+    if (!this.get('media.isSmall') && !this.get('media.isMedium')) {
+      this.set('classOpened', '');
+      return;
+    }
+
+    if (this.get('isShown')) {
+      this.set('classOpened', 'home-faq__question--opened');
+      return;
+    }
+
+    if (!this.get('isShown')) {
+      this.set('classOpened', 'home-faq__question--closed');
+      return;
+    }
+  }),
+
+  didInsertElement() {
+    this.setupIsShown();
+  },
+
+  setupIsShown() {
+    if (this.get('media.isSmall') || this.get('media.isMedium')) {
       this.set('isShown', false);
       return;
     }
 
     this.set('isShown', true);
-  }),
-
-  didInsertElement() {
   },
 
   actions: {
     onClickTitle() {
-      if (!this.get('media.isSmall')) {
+      if (!this.get('media.isSmall') && !this.get('media.isMedium')) {
         return;
       }
 
