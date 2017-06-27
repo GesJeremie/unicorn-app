db30aa51c0ce3cddef723e1a01e52f82610c2f9e
Jeremie Ges <ges.jeremie@gmail.com>
2017-06-28 02:07:49 +0300

    Refacto song item component

--- app/components/unicorn-join/song-item.js
+++ app/components/unicorn-join/song-item.js
@@ -2,11 +2,31 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  /**
+   * The song item
+   */
+  song: null,
+
+  /**
+   * Setup the component
+   */
   didInsertElement() {
+    this.setupDefaultPushButtonLabel();
+  },
+
+  /**
+   * Set a label to the push button
+   */
+  setupDefaultPushButtonLabel() {
     this.set('song.pushButtonLabel', 'Push Song');
   },
 
   actions: {
+
+    /**
+     * Push song clicked, call the handler given
+     * @param {Object} song
+     */
     onClickPushSong(song) {
       this.get('onPushSong')(song);
     }
