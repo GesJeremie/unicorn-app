a81a21e4bbc6622071cc51b6b89a1f7250a6f6bd
Jeremie Ges <ges.jeremie@gmail.com>
2017-06-28 02:10:17 +0300

    Refacto search tracks

--- app/components/unicorn-join/search-tracks.js
+++ app/components/unicorn-join/search-tracks.js
@@ -2,33 +2,46 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  /**
+   * Services
+   */
   store: Ember.inject.service(),
 
-  search: null,
-  searching: false,
+  /**
+   * The current search
+   */
+  search: '',
+
+  /**
+   * Check if a search is performing
+   */
+  isSearching: false,
+
+  /**
+   * Songs retrieved from the search
+   */
   songs: null,
 
-  didInsertElement() {
-  },
+  didInsertElement() {},
 
   actions: {
+
+    /**
+     * Search the song for the search given
+     */
     searchSong() {
       let search = this.get('search');
 
-      this.set('searching', true);
+      this.set('isSearching', true);
 
       this.get('store').query('song', {query: search})
       .then((response) => {
-        this.set('searching', false);
+        this.set('isSearching', false);
         this.set('songs', response)
       })
       .catch((error) => {
-        this.set('searching', false);
+        this.set('isSearching', false);
       });
     }
-
-    /*onClickPushSong(song) {
-      this.get('push')(song);
-    }*/
   }
 });
